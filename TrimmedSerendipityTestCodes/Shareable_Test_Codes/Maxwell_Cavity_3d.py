ff20a4d12b53e24e3b3295679da79e95844bc8f4
Justincrum <jcrum@email.arizona.edu>
2021-03-19 15:28:21 -0700

    Adding in header comments and license.

--- TrimmedSerendipityTestCodes/Shareable_Test_Codes/Maxwell_Cavity_3d.py
+++ TrimmedSerendipityTestCodes/Shareable_Test_Codes/Maxwell_Cavity_3d.py
@@ -1,3 +1,23 @@
+#file:  Maxwell_Cavity_3d.py
+#author:  Justin Crum
+#date:  3/19/21
+#
+#
+# This code does an eigenvalue solve using Firedrake
+"""
+
+Copyright 2021 Justin Crum
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+"""
+
 from firedrake import *
 from petsc4py import PETSc
 from slepc4py import SLEPc
@@ -12,7 +32,7 @@ for PolyDegree in range(2, 3):
         N = 2 ** j
         msh = RectangleMesh(N, N, np.pi, np.pi, quadrilateral=True)
         mesh = ExtrudedMesh(msh, layers=N, layer_height=np.pi/(N)) 
-
+        
         HCurlSpace = FunctionSpace(mesh, "SminusCurl", PolyDegree)
         u = TrialFunction(HCurlSpace)
         v = TestFunction(HCurlSpace)
